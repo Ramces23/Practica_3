09a6d044e63cbc1d6185465f2dff4f7010c468f9
ramces batista peña <ramcesbape@hotmail.com>
2021-02-11 16:05:56 -0400

    ejercicio 1entrada

---
+++ Ejercicio 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ejercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa desarrollado por Ramces Batista Peña para numeros positivos");
+            double Num = 0;
+            double N = 0;
+            Console.WriteLine("Digite su numero positivo para sumar, digite un numero negativo o cero para dejar de sumar");
+            do
+            {
+                Num = Convert.ToDouble(Console.ReadLine());
+                N = N + Num;
+
+            } while (Num > 0);
+            Console.WriteLine("La suma de los numeros digitados es: " + N);
+        }
+    }
+}
