6ca20de8b09f51a5f1c413cb8b15b9d46e325852
Ramces23 <73758593+Ramces23@users.noreply.github.com>
2021-02-11 17:48:34 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Ejercicio_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa desarrollado por Ramces Batista Peña para las letras de la Z a la A ");
+
+            Char L;
+            for (L = 'Z'; L >= 'A'; L--)
+                Console.WriteLine(" [{0}]", L);
+        }
+    }
+}
