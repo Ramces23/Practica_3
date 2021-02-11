0c9c51fd0fc54e7913576e117d77852aaaaa7063
SONY <SONY@DESKTOP-B5DNI1I>
2021-02-11 15:34:15 -0400

    Practica 3 Agregar .gitignore y .gitattributes.

---
+++ Ejercicio_2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ejercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa creado por Ramces Batista Peña para contar desde el 1 al 10 ");
+            int Numeros = 0;
+            do
+            {      
+                Numeros = Numeros + 1;
+                Console.WriteLine(Numeros);
+
+            }while (Numeros < 10) ;
+        }
+    }
+}
---
+++ Ejercicio_3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ejercicio_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa creado por Ramces Batista Peña para que muestre los primeros 10 números pares a partir del producto de (10 x 10)");
+            for (int N = 10 * 10; N <= ((10 * 10) + 20); N += 2)
+            {
+              
+                Console.WriteLine(" [{0}]", N);
+
+            }
+        }
+    }
+}
